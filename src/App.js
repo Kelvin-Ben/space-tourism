ee1f0a19c005043318133d0dcd172d64d90beab1
Kelvin-Ben <kevoben14@gmail.com>
2024-04-30 10:38:37 +0300

    Add a few routes for different components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 
+import Root from "./components/Root";
+import Home from "./components/home/home";
+import Destination from "./components/destination/Destination";
+import Crew from "./components/crew/Crew";
+import Technology from "./components/technology/Technology";
+import classes from "./App.module.css";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route path="/" element={<Home />} />
+      <Route path="destination" element={<Destination />} />
+      <Route path="crew" element={<Crew />} />
+      <Route path="technology" element={<Technology />} />
+    </Route>
+  )
+);
 const App = () => {
   return (
-    <><h1>This is the beginning!</h1></>
-  )
-}
+    <main className={classes.main}>
+      <RouterProvider router={router} />
+    </main>
+  );
+};
 
-export default App
+export default App;
