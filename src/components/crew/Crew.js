98b2c4e9d019035b3093287983d07ac342a4be25
Kelvin-Ben <kevoben14@gmail.com>
2024-05-25 12:36:57 +0300

    Add backImages through state management

--- src/components/crew/Crew.js
+++ src/components/crew/Crew.js
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Crew.module.css";
 
 const Crew = () => {
-  return <div className={styles.container}>crew</div>;
+  const [backgroundImage, setBackgroundImage] = useState("");
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 1024) {
+        setBackgroundImage(
+          require("../../assets/crew/background-crew-desktop.jpg")
+        );
+      } else if (window.innerWidth >= 768) {
+        setBackgroundImage(
+          require("../../assets/crew/background-crew-tablet.jpg")
+        );
+      } else {
+        setBackgroundImage(
+          require("../../assets/crew/background-crew-mobile.jpg")
+        );
+      }
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  return (
+    <div
+      className="bg-cover bg-center h-screen text-customWhite"
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+      }}
+    >
+      <h1>Crew Page</h1>
+    </div>
+  );
 };
 
 export default Crew;
