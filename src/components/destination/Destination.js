17ef11139aa3d5b7c956e5c0acce1c692b4c69f3
Kelvin-Ben <kevoben14@gmail.com>
2024-06-02 16:24:30 +0300

    Improved performance for the destination page

--- src/components/destination/Destination.js
+++ src/components/destination/Destination.js
@@ -37,9 +37,9 @@ const Destination = () => {
         backgroundImage: `url(${backgroundImage})`,
       }}
     >
-      <div className="relative 2xl:w-[80%] 2xl:top-[25%] md:w-[90%] md:top-[15%] min-h-80 lg:top-1/4 mx-auto sm:top-[10%] sm:w-auto xl:top-[25%] xl:w-[90%]">
+      <div className="relative 2xl:w-[80%] 2xl:top-[25%] md:w-[90%] md:top-[15%] min-h-80 lg:top-1/4 mx-auto sm:top-[10%] sm:w-auto sm:h-[80vh] xl:top-[25%] xl:w-[90%]">
         <h1 className=" flex text-4xl uppercase tracking-widest mb-20 font-barlow sm:justify-center sm:text-4xl sm:mb-10 lg:w-[90%] lg:mx-auto">
-          <span className="w-20 inline-block text-customGray font-bold">
+          <span className="w-20 inline-block text-customDark font-bold">
             01
           </span>
           pick your destination
@@ -49,17 +49,17 @@ const Destination = () => {
             dest.name === activeTab && (
               <article
                 key={dest.name}
-                className="lg:w-[90%] lg:mx-auto lg:min-h-[50%] lg:flex lg:flex-row 2xl:flex 2xl:justify-around xl:flex md:w-full items-center justify-around sm:px-10 sm:w-full"
+                className="lg:w-[90%] lg:mx-auto lg:min-h-[50%] lg:flex lg:flex-row 2xl:flex 2xl:justify-between 2xl:mx-auto 2xl:h-[50rem] xl:flex md:w-full items-center justify-around sm:px-10 sm:w-full 2xl:w-[90%] "
               >
-                <div className="lg:w-auto lg:justify-normal md:w-full md:flex md:justify-center max-h-[80%] sm:justify-center sm:w-[100%]">
+                <div className="lg:w-auto lg:justify-normal md:w-full md:flex md:justify-center max-h-[80%] sm:justify-center sm:w-[100%] sm:h-[24rem] sm:items-center">
                   <img
                     src={require(`../../${dest.images.webp}`)}
                     alt={`destination ${activeTab.toLowerCase()}`}
-                    className="lg:w-[75%] md:w-[40%] flex-initial md:my-10 md:p-4 sm:mx-auto sm:mb-10 sm:w-[50%]"
+                    className="lg:w-[75%] md:w-[40%] flex-initial md:my-10 md:p-4 sm:mx-auto  sm:w-[60%]"
                   />
                 </div>
-                <div className="lg:w-[50%] lg:h-full md:w-full md:text-center 2xl:w-[50%] xl:w-[50%] 2xl:pl-[10rem]">
-                  <div className="w-full flex justify-end mx-auto items-end md:min-h-10 2xl:justify-start xl:justify-start lg:justify-start ">
+                <div className="lg:w-[50%] h-full md:w-full md:text-center 2xl:w-[50%] xl:w-[50%] 2xl:pl-[10rem]">
+                  <div className="w-full flex justify-end mx-auto items-end md:h-[5rem] 2xl:justify-start xl:justify-start lg:justify-start">
                     <ul className="flex gap-14 md:w-full font-barlow uppercase tracking-widest list-none font-normal py-4 text-3xl lg:justify-normal md:justify-center sm:mx-auto sm:text-2xl sm:w-full sm:justify-center">
                       {data.destinations.map((dest) => (
                         <li key={dest.name}>
@@ -67,7 +67,7 @@ const Destination = () => {
                             className={`${
                               activeTab === dest.name
                                 ? "underline underline-offset-4 decoration-2 uppercase"
-                                : "text-customBlue uppercase"
+                                : "text-customDark uppercase"
                             }`}
                             onClick={() => setActiveTab(dest.name)}
                           >
@@ -80,7 +80,7 @@ const Destination = () => {
                   <h2 className="flex lg:justify-normal font-bellefair uppercase text-5xl md:justify-center md:text-[8rem] sm:justify-center sm:text-[6rem] sm:w-full 2xl:text-[7.5rem] xl:text-[7.5rem]">
                     {dest.name}
                   </h2>
-                  <p className="lg:w-[75%] lg:text-start lg:text-2xl font-barlow mb-20 md:text-3xl text-customBlue md:w-[90%] md:mx-auto md:align-middle tracking-wider sm:w-[90%] sm:mx-auto sm:text-2xl sm:text-center xs:text-2xl xs:w-full xs:text-center 2xl:w-[65%] xl:w-[70%] 2xl:text-3xl xl:text-3xl">
+                  <p className="lg:w-[75%] lg:text-start lg:text-2xl font-barlow mb-20 md:text-3xl text-customBlue md:w-[80%] md:mx-auto md:align-middle tracking-wider sm:w-[90%] sm:mx-auto sm:text-3xl sm:text-center xs:text-2xl xs:w-full xs:text-center 2xl:w-[65%] xl:w-[70%] 2xl:text-3xl xl:text-3xl">
                     {dest.description}
                   </p>
                   <div className="bg-[#ffffff4d] h-[.1rem] md:w-[90%] md:mx-auto mb-10 2xl:w-[70%] xl:w-[70%] lg:w-[75%]"></div>
